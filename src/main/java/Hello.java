6bc174474a096950d19e4a1c9429c6f1d3d9a255
Yoyo <8448220+yoyo727399@user.noreply.gitee.com>
2023-03-26 13:54:24 +0800

    commit3

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("master1");
         System.out.println("master2");
+        System.out.println("master3");
     }
 }
